aac5698b2a05ab78efec906c1242c1366c304b2b
tianyu <779205344@qq.com>
2024-04-04 12:37:19 +0800

    fix: 更新依赖包

--- docs-src/example/base.tsx
+++ docs-src/example/base.tsx
@@ -5,12 +5,27 @@ import {
   SandpackProvider,
 } from '@codesandbox/sandpack-react'
 import SandpackFileExplorer from '@rainetian/sandpack-file-explorer'
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [template, setTemplate] = useState<any>('')
+
+  const handleSelectProject = () => {
+    if (template === 'react-ts') {
+      setTemplate('vue')
+    } else if (template === 'vue') {
+      setTemplate('static')
+    } else if (template === 'static') {
+      setTemplate('react-ts')
+    } else {
+      setTemplate('react-ts')
+    }
+  }
+
   return (
     <div>
-      <SandpackProvider template='react'>
+      <button onClick={handleSelectProject}>change</button>
+      <SandpackProvider template={template}>
         <SandpackLayout>
           <SandpackFileExplorer style={{ width: 250 }} />
           <SandpackCodeEditor showTabs closableTabs />
